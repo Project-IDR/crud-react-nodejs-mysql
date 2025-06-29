da91d84bcbc3937439c00c18d19209212bcc625f
Dony Cahyanto <donycahyanto99@gmail.com>
2025-06-29 15:40:02 +0700

    tambah navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import Table from "./components/Table";
 import "./styles/App.css"
 function App() {
   return (
     <>
+      <Navbar/>
       <Table />
     </>
   );
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
+      <div className="container-fluid px-5">
+        <a className="navbar-brand" href="#">PROJECT IDR $</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
